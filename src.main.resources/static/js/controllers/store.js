59632c996ddf286f72d420ade93f9e3170c5a096
kimdoyong <kdy10041004@naver.com>
2016-02-09 19:19:42 +0900

    7-8 장바구니 위젯 추

--- src.main.resources/static/js/controllers/store.js
+++ src.main.resources/static/js/controllers/store.js
@@ -1,11 +1,11 @@
-angular.module("store", ["customFilters"])
+angular.module("store", ["customFilters", "cart"])
 .constant("dataUrl","http://localhost:8080/products")
 .controller("storeCtrl", function($scope, $http, dataUrl) {
 
 	$scope.data = {};
 	$http.get(dataUrl)
     .success(function (data) {
-        $scope.data.products = data;
+    	$scope.data.products = data;
     })
     .error(function (error) {
         $scope.data.error = error;
